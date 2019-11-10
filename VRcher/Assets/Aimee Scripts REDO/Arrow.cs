6ba4dbefa211d0d04537cfe78f2fd2a7c836d541
Aimee-Mattingley <19046244@brookes.ac.uk>
2019-11-10 23:30:01 +0000

    Attempting to Make the BOW and Arrow Work
    Created new scenes with attempts (main one back up)

---
+++ VRcher/Assets/Aimee Scripts REDO/Arrow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+    void OnTriggerStay() {
+        AttachArrow();
+    }
+
+    void OnTriggerEnter() {
+        AttachArrow();
+    }
+
+    private void AttachArrow() {
+        var device = SteamVR_Controller.Input((int)ArrowManagerNew.Instance.trackedObj.index);
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger)) {
+            ArrowManagerNew.Instance.AttachBowToArrow ();
+        }
+    }
+
+}
---
+++ VRcher/Assets/Aimee Scripts REDO/ArrowManagerNew.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowManagerNew : MonoBehaviour {
+
+    public static ArrowManagerNew Instance;
+
+    public SteamVR_TrackedObject trackedObj;
+
+    private GameObject currentArrow;
+
+    public GameObject stringAttachPoint;
+    public GameObject arrowStartPoint;
+    public GameObject stringStartPiont;
+
+    public GameObject arrowPrefab;
+
+    private bool isAttached = false;
+
+    void Awake() {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    void OnDestroy() {
+        if (Instance == this)
+            Instance = null;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    void Update() {
+        AttachArrow();
+        PullString();
+    }
+
+    private void PullString() {
+        if (isAttached) {
+            float dist = (stringStartPiont.transform.position - trackedObj.transform.position).magnitude;
+            stringAttachPoint.transform.localPosition = stringStartPiont.transform.localPosition + new Vector3(0f, dist, 0f);
+        }
+}
+
+private void AttachArrow() {
+        if (currentArrow == null) {
+            currentArrow = Instantiate (arrowPrefab);
+            currentArrow.transform.parent = trackedObj.transform;
+            currentArrow.transform.localPosition = new Vector3 (0f, -.03f, .01f);
+        }
+    }
+
+    public void AttachBowToArrow() {
+        currentArrow.transform.parent = stringAttachPoint.transform;
+        currentArrow.transform.localPosition = arrowStartPoint.transform.localPosition;
+        currentArrow.transform.rotation = arrowStartPoint.transform.rotation;
+
+        isAttached = true;
+    }
+}
