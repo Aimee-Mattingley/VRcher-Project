a39b1ecfe32e6e45f7b501b26de38190eb971105
Nejc Puska <19042119@brookes.ac.uk>
2019-12-09 15:41:31 +0000

    Enemy waypoints fixed
    Enemies now follow random waypoints to reach the tower

--- VRcher/Assets/Nejc's assets/Scripts/EnemyMovement.cs
+++ VRcher/Assets/Nejc's assets/Scripts/EnemyMovement.cs
@@ -8,7 +8,8 @@ public class EnemyMovement : MonoBehaviour
     private Transform target;
     private int wavepointIndex = 0;
     private int waypointChooser = 0;
-    
+    public System.Random r = new System.Random();
+    public int rInt;
     Animator anim;
 
 	void Awake ()
@@ -18,31 +19,62 @@ public class EnemyMovement : MonoBehaviour
     
     void Start()
     {
-        target = Waypoints.points[0];
+        rInt = r.Next(0,3);
+        if(rInt == 0){
+            target = Waypoints.points[0];
+        }
+        else if (rInt == 1){
+            target = LeftWaypoints.points[0];
+        }
+        else if (rInt == 2){
+            target = RightWaypoints.points[0];
+        }
         anim.SetBool("isRun",!(anim.GetBool("isRun")));
     }
 
     void Update()
     {
+
         Vector3 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
     
         if(Vector3.Distance(transform.position, target.position) <= 0.4f)
         {
-            GetNextWaypoint();
+            if(rInt == 0){GetNextWaypoint();}
+            else if (rInt == 1){GetNextLeftWaypoint();}
+            else if (rInt == 2){GetNextRightWaypoint();}
         }
     }
 
     void GetNextWaypoint()
     {
-        if(wavepointIndex >= Waypoints.points.Length - 1)
-        {
+        if(wavepointIndex >= Waypoints.points.Length - 1){
             Destroy(gameObject);
             return;
         }
 
         wavepointIndex++;
         target = Waypoints.points[wavepointIndex];
+    }
+    void GetNextLeftWaypoint()
+    {
+        if(wavepointIndex >= LeftWaypoints.points.Length - 1){
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        target = LeftWaypoints.points[wavepointIndex];
+    }
+    void GetNextRightWaypoint()
+    {
+        if(wavepointIndex >= RightWaypoints.points.Length - 1){
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+        target = RightWaypoints.points[wavepointIndex];
     }
 
 }
--- VRcher/Assets/Nejc's assets/Scripts/RightWaypoints.cs
+++ VRcher/Assets/Nejc's assets/Scripts/RightWaypoints.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class Waypoints : MonoBehaviour
+public class RightWaypoints : MonoBehaviour
 {
 
     public static Transform[] points;
